6403bae0649b9f1feb63d60531aa619ddaa7725a
selinapn <57070340+selinapn@users.noreply.github.com>
2021-01-16 20:35:05 -0800

    removed comments

--- automation/functions/script.ps1
+++ automation/functions/script.ps1
@@ -1,7 +1,6 @@
 #sign into azure account 
 Connect-AzAccount
 
-#create unique name for HTTP Trigger function
 
 $resourceGroupName = Read-Host -Prompt "Name your resource group"
 $location = Read-Host -Prompt "Enter location"
@@ -10,8 +9,3 @@ $templateUri = "https://raw.githubusercontent.com/selinapn/ad440-winter2021-tues
 #Create new resource group and deploy template
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri
-
-
-<# publish function to azure
-$functionappname = Read-Host -Prompt "Name your function APP"
-func azure functionapp publish $functionappname #>
