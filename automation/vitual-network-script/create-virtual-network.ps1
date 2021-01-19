6726907e6fff3fda87c6ecfb05edc7633505e8ea
Jennvillacis <jennvillacis@gmail.com>
2021-01-18 19:07:10 -0800

    updates to create vnet script
    creates a virtual network with subnet

--- automation/vitual-network-script/create-virtual-network.ps1
+++ automation/vitual-network-script/create-virtual-network.ps1
@@ -1,40 +1,58 @@
+{
 [cmdletbinding()]
- Param(
+Param(
     [Parameter(Mandatory=$true)]
+    [string]
     $SubscriptionId,
     [Parameter(Mandatory=$true)]
-    $rgName,
+    [string]
+    $RGName,
     [Parameter(Mandatory=$true)]
+    [string]
     $VNetName,
-    $SubNetName,
+    [Parameter(Mandatory=$true)]
+    [string]
     $VNetPrefix,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $SubNetName,
+    [Parameter(Mandatory=$true)]
+    [string]
     $SubVNetPrefix
    )
 
+ "you entered $SubscriptionId" 
+ "you entered $RGName"   
+ "you entered $SubNetName"   
+ "you entered $SubVNetPrefix"   
+ "you entered $VNetName"   
+
 #Login into Azure
-Login-AzAccount | Out-Null
+Connect-AzAccount | Out-Null
 
 #Selecting subscription
 Select-AzureSubscription -SubscriptionId $SubscriptionId -ErrorAction Stop
 
-
 #Selecting resource
-Get-AzResourceGroup -ResourceGroupName $rgName
-
-$Location = (Get-AzureRmResourceGroup -Name $rgName).location
+Get-AzResourceGroup -ResourceGroupName $RGName
 
 # Create a virtual network
 $virtualNetwork = New-AzVirtualNetwork `
-  -ResourceGroupName $RG `
-  -Location $Location `
-  -Name $VNetName `
+  -ResourceGroupName $RGName 
+  -Location $Location
+  -Name $VNetName 
+  "this is address pre-fix for vnet $VNetPrefix"
   -AddressPrefix $VNetPrefix
 
 # Create a subnet configuration
-$subnetConfig = Add-AzVirtualNetworkSubnetConfig `
-  -Name $SubNetName `
-  -AddressPrefix 10.0.0.0/24 `
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+  -Name $SubNetName 
   -VirtualNetwork $virtualNetwork
+  -AddressPrefix $SubVNetPrefix
+
+$virtualNetwork.Subnets.Add($subnetConfig)  
 
 #Associates the subnet to the virtual network
   $virtualNetwork | Set-AzVirtualNetwork
+
+}
