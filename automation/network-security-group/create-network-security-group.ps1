24a3c7199172a452f96d873c9829d7bb6d1680cd
OyunominJuno <oyunominmun@gmail.com>
2021-01-18 22:13:54 -0800

    Added nsg creating script and directories

---
+++ automation/network-security-group/create-network-security-group.ps1
@@ -0,0 +1,32 @@
+#this script creates a network secutiry group to an existing resource group
+param(
+    [Parameter(Mandatory=$True)]
+    [string]
+    $SubscriptionId,     #Subscription ID to use
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $TenantId,           #Tenant ID to use
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $ServicePrincipalId,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $ServicePrincipalPassword
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $ResourceGroupName   #Resource group name to add the network secutiry group into
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $Location            #Resource group location
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $NetworkSecurityGroupName  #Network Secutiry Name to create
+)
+
+New-AzNetworkSecurityGroup -Name $NetworkSecurityGroupName -ResourceGroupName $ResourceGroupName  -Location  $Location
