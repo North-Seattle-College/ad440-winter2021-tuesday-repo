05856f07e40ba18e968403ce465768d17d17f016
Phil Kim <philipkim206@gmail.com>
2021-01-18 16:38:30 -0800

    issues galore and not sure what's happening

--- functions/Utils/RequestHandler.py
+++ functions/Utils/RequestHandler.py
@@ -1,5 +1,87 @@
+from ..Model.User import User
+from ..Model.Task import Task
+import azure.functions as func
+from .ExceptionWithStatusCode import ExceptionWithStatusCode
 
-class ExceptionWithStatusCode(Exception):
-    def __init__(self, msg, status_code):
-        super(ExceptionWithStatusCode, self).__init__(msg)
-        self.status_code = status_code
+class RequestHandler():
+
+    def getUserId(self, req: func.HttpRequest) -> UserId:
+        req_body = req.form
+
+        # get id (required field)
+        try:
+            id = req_body['id']
+        except KeyError:
+            id = None
+        
+        # get email (required field)
+        try:
+            email = req_body['email']
+        except KeyError:
+            raise ExceptionWithStatusCode(
+                'Email is required',
+                status_code=400
+            )
+        if len(title) == 0:
+            raise ExceptionWithStatusCode(
+                'Email cannot be empty',
+                status_code=400
+            )
+
+        # get userPassword (required field)
+        try:
+            userPassword = req_body['userPassword']
+        except KeyError:
+            raise ExceptionWithStatusCode(
+                'Email is required',
+                status_code=400
+            )
+        if len(title) == 0:
+            raise ExceptionWithStatusCode(
+                'Email cannot be empty',
+                status_code=400
+            )
+
+        # get firstName (required field)
+        try:
+            firstName = req_body['firstName']
+        except KeyError:
+            raise ExceptionWithStatusCode(
+                'First name is required',
+                status_code=400
+            )
+        if len(title) == 0:
+            raise ExceptionWithStatusCode(
+                'First Name cannot be empty',
+                status_code=400
+            )
+
+        # get lastName (required field)
+        try:
+            lastName = req_body['lastName']
+        except KeyError:
+            raise ExceptionWithStatusCode(
+                'Last Name is required',
+                status_code=400
+            )
+        if len(title) == 0:
+            raise ExceptionWithStatusCode(
+                'Last Name cannot be empty',
+                status_code=400
+            )
+
+        # get joinDate (required field)
+        try:
+            joinDate = req_body['joinDate']
+        except KeyError:
+            raise ExceptionWithStatusCode(
+                'Join Date is required',
+                status_code=400
+            )
+        if len(title) == 0:
+            raise ExceptionWithStatusCode(
+                'Join Date cannot be empty',
+                status_code=400
+            )
+
+        return UserId(id, email, password, firstName, lastName, joinDate)
--- functions/Utils/dbHandler.py
+++ functions/Utils/dbHandler.py
@@ -1,11 +1,31 @@
+import os
+import pyodbc
+from ..Model.User import User
+from ..Model.Task import Task
+from typing import List
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVER = os.environ['SERVER']
+DATABASE = os.environ['DATABASE']
+USERNAME = os.environ['USER']
+PASSWORD = os.environ['PASSWORD']
+DRIVER = os.environ['DRIVER']
+
 class dbHandler():
     def __init__(self):
+        def __init__(self):
+            self.conn_string = 'DRIVER=' + DRIVER + \
+                ';SERVER=' + SERVER + \
+                ';PORT=1433;DATABASE=' + DATABASE + \
+                ';UID='+USERNAME+';PWD='+PASSWORD
 
     # [Task # 2] Query and return all user_id's from SQL DB
-    def getUserIds(self) -> List[User_Id]:
+    def getUserIds(self) -> List[User]:
         with pyodbc.connect(self.conn_string) as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT * FROM UserID")
+                cursor.execute("SELECT * FROM Users")
                 rows = cursor.fetchall()
         user_id = []
         for row in rows:
@@ -18,11 +38,12 @@ class dbHandler():
             with conn.cursor() as cursor:
                 cursor.execute(
                     """
-                    INSERT INTO User_Id (title, description, status, id)
-                    VALUES (?, ?, ?, ?)
+                    INSERT INTO User_Id (id, email, userPassword, firstName, lastName, joinDate)
+                    VALUES (?, ?, ?, ?, ?, ?)
                     """,
-                    user_id.id, user_id.firstName, user_id.lastName, user_id.email, user_id.status
+                    user_id.id, user_id.email, user_id.userPassword, user_id.firstName, user_id.lastName, user_id.joinDate
                 )
+                # add return messaging (user ID) back to user
 
     # [Task # 2] Delete user_id from SQL DB
     def deleteUserId(self, user_id: User_Id) -> None:
@@ -30,8 +51,9 @@ class dbHandler():
             with conn.cursor() as cursor:
                 cursor.execute(
                     """
-                    DELETE FROM User_Id (title, description, status, id)
-                    VALUES (?, ?, ?, ?)
+                    DELETE FROM User_Id (id, email, userPassword, firstName, lastName, joinDate)
+                    VALUES (?, ?, ?, ?, ?, ?)
                     """,
-                    user_id.id, user_id.firstName, user_id.lastName, user_id.email, user_id.status
+                    user_id.id, user_id.email, user_id.userPassword, user_id.firstName, user_id.lastName, user_id.joinDate
                 )
+                # add messaging 200 or proper exec
