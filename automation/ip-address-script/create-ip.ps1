7d87367acf43aa1368696c84dabcc5a0da831055
Derek Hendrick <phantom.dev.elb@gmail.com>
2021-01-18 22:51:26 -0800

    Adds dynamic location

--- automation/ip-address-script/create-ip.ps1
+++ automation/ip-address-script/create-ip.ps1
@@ -19,7 +19,11 @@ param (
 
     [Parameter(Mandatory=$True)]
     [string]
-    $IpName
+    $IpName,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $Location
 )
 
 #To login to azure from powershell use the following
@@ -29,10 +33,7 @@ Connect-AzAccount
   #  $Credential = Get-Credential
   #  Connect-AzAccount -Credential $Credential -Tenant 'xxxx-xxxx-xxxx-xxxx' -ServicePrincipal
 
-#Will need to modify this to accept a location
-$location = "westus2"
-
 #deploying IP address
-New-AzPublicIpAddress -Name $ipName -ResourceGroupName nsc-rg-dev-usw2-team3 -Location $location -AllocationMethod Dynamic -IpAddressVersion IPv4
+New-AzPublicIpAddress -Name $IpName -ResourceGroupName nsc-rg-dev-usw2-team3 -Location $Location -AllocationMethod Dynamic -IpAddressVersion IPv4
 
 #For the most part, few things need to be dynamically set by the user for this, so far.
