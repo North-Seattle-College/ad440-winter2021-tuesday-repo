68733804d3d9c87c51cc8f6d532743f0929d51da
Asia <Joanna.gromadzka@seattlecentral.edu>
2021-01-19 15:35:27 -0800

    creates only VM, disk and sg

--- automation/vm/deploy-vm.ps1
+++ automation/vm/deploy-vm.ps1
@@ -1,70 +1,69 @@
-[CmdletBinding()]
-param (
+<#
+.SYNOPSIS
+  Creates a virtual machine with one line cmdlet.
+.DESCRIPTION
+  This script will deploy a virtual machine. 
+  This script  requires an existing Azure subscription and Resource group.
+.PARAMETER ResourceGroupName
+    Required. Name of Resource group, if already exists will ask if add VM to it   
+.PARAMETER Location
+    Required. Name of location of Resource eg: westus2     
+.PARAMETER VNetName
+    Required. Name of desired Virtual Network Name Eg: [project]-[resource-type]-[environment]-[location]-[other-stuff]
+.PARAMETER SubNetName
+    Required. Name of desired SubNet
+.PARAMETER VMname
+    Required. Desired name for virtual machine
+.PARAMETER SecurityGroupName
+    Required. Desired name for security group
+.PARAMETER PublicIpAddressName
+    Required. Desired name for public Ip address
+.NOTES
+  Version:        1.0
+  Author:         Joanna Gromadzka
+  Creation Date:  01/17/21
+  Purpose: VM creation Script
+
+  This script requires to obtain administrator's name and strong password (newly made is ok)
+#>
+[cmdletbinding()]
+Param(
     [Parameter(Mandatory=$True)]
     [string]
     $SubscriptionId,
-
     [Parameter(Mandatory=$True)]
     [string]
     $TenantId,
-
-    [Parameter(Mandatory=$False)]
+    [Parameter(Mandatory=$true)]
     [string]
-    $ServicePrincipalId,
-
-    [Parameter(Mandatory=$False)]
-    [SecureString]
-    $ServicePrincipalPassword,
-
-    [Parameter(Mandatory=$True)]
-    [string]
-    $RGName,
-    
-    [Parameter(Mandatory=$True)]
-    [string]
-    $Location,
-
-    [Parameter(Mandatory=$True)]
-    [string]
-    $TempFilePath,
-
-    [Parameter(Mandatory=$True)]
-    [string]
-    $ParamFilePath,
-
-    [Parameter(Mandatory=$True)]
+    $ResourceGroupName,
+    [Parameter(Mandatory=$true)]
     [string]
     $VMName,
-
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$true)]
     [string]
-    $vnet,
-
-    [Parameter(Mandatory=$True)]
+    $Location,
+    [Parameter(Mandatory=$true)]
     [string]
-    $subnet,
-
-    [Parameter(Mandatory=$True)]
+    $VNetName,
+    [Parameter(Mandatory=$true)]
     [string]
-    $sgname,
-
-    [Parameter(Mandatory=$True)]
+    $SubNetName,
+    [Parameter(Mandatory=$true)]
     [string]
-    $ipaddress
-    
-)
-
-#To login to azure from powershell use the following
+    $SecurityGroupName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $PublicIpAddressName
+   )
+#Login into Azure
 Connect-AzAccount 
-#to Connect to Azure using a service principal account
-    
-  #  $Credential = Get-Credential
-  #  Connect-AzAccount -Credential $Credential -Tenant 'xxxx-xxxx-xxxx-xxxx' -ServicePrincipal
-    
-  
 
-#first thing to create a resource group
-#New-AzResourceGroup -ResourceGroupName $RGName -Location $Location
+#Display existing typed resource group
+Get-AzResourceGroup -ResourceGroupName $ResourceGroupName
 
-#deploying VM(created all resources)
-New-AzVM -ResourceGroupName $RGName -Name $VMName -Location $Location -VirtualNetworkName $vnet -SubnetName $subnet -SecurityGroupName $sgname -PublicIpAddressName $ipaddress 
+#creates new resource group if needed
+#New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+#creates VM
+New-AzVm -ResourceGroupName $ResourceGroupName -Name $VMName -Location $Location -VirtualNetworkName $VNetName -SubnetName $SubNetName -SecurityGroupName $SecurityGroupName -PublicIpAddressName $PublicIpAddressName -OpenPorts 80,3389
