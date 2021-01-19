83f488d44877c77f5e6ca801b270ee50e26b20a6
Raotrux <56573919+Raotrux@users.noreply.github.com>
2021-01-19 15:06:46 -0800

    Updated conn_string
    Removed the hard coded server to the SERVER environ variable.

--- functions/Utils/dbHandler.py
+++ functions/Utils/dbHandler.py
@@ -10,9 +10,10 @@ PASSWORD = os.environ.get('PASSWORD')
 DRIVER = os.environ.get('DRIVER')
 
 
+
 class dbHandler():
     def __init__(self):
-        self.conn_string = "Driver={" + DRIVER + "};Server=tcp:nsc-sqlsrv-usw2-sqltest.database.windows.net,1433;Database=" + DATABASE + ";Uid=" + USERNAME + ";Pwd={" + PASSWORD + "};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+        self.conn_string = "Driver={" + DRIVER + "};Server=" + SERVER + ";Database=" + DATABASE + ";Uid=" + USERNAME + ";Pwd={" + PASSWORD + "};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
         '''
         'Driver=' + DRIVER + \
             ';Server=' + SERVER + \
