7c72ae90933ec31a7ede2d31338c3574357ba9c6
Jennvillacis <jennvillacis@gmail.com>
2021-01-18 23:38:37 -0800

    Update deploy-vnet.ps1
    script changes

--- automation/vnet/deploy-vnet.ps1
+++ automation/vnet/deploy-vnet.ps1
@@ -4,18 +4,18 @@
 .DESCRIPTION
   This script will deploy a virtual network. 
   This script  requires an existing Azure subscription and Resource group.
-.PARAMETER SubscriptionId
-    Required. Subscription ID of Azure Account
 .PARAMETER RGName
-    Required. Name of existing Resource 
+    Required. Name of existing Resource  
+.PARAMETER Location
+    Required. Name of location of Resource eg: westus2     
 .PARAMETER VNetName
     Required. Name of desired Virtual Network Name Eg: [project]-[resource-type]-[environment]-[location]-[other-stuff]
 .PARAMETER VNetPrefix
-    Required. Desired address space for Virtual Network
+    Required. Desired address space for Virtual Network eg: eg: 10.0.0.0/16
 .PARAMETER SubNetName
     Required. Name of desired SubNet
 .PARAMETER SubVNetPrefix
-    Required. Desired address space for SubNet
+    Required. Desired address space for SubNet eg: 10.0.0.0/24
 .NOTES
   Version:        1.0
   Author:         Jennifer Villacis
@@ -25,9 +25,6 @@
 
 [cmdletbinding()]
 Param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $SubscriptionId,
     [Parameter(Mandatory=$true)]
     [string]
     $RGName,
@@ -51,10 +48,7 @@ Param(
 #Login into Azure
 Connect-AzAccount | Out-Null
 
-#Selecting subscription
-Select-AzureSubscription -SubscriptionId $SubscriptionId -ErrorAction Stop
-
-#Display and Select resource
+#Display existing typed resource group
 Get-AzResourceGroup -ResourceGroupName $RGName
 
 # Create a subnet configuration
