75733b2a28ad014fe7b818b3d9bb42df9443ee86
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2021-01-20 12:46:24 -0800

    Refactor tasks endpoint

--- functions/GetTasks/__init__.py
+++ functions/GetTasks/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import pyodbc
 
 import azure.functions as func
 from ..Utils.dbHandler import dbHandler
@@ -8,11 +9,31 @@ from ..Utils.ExceptionWithStatusCode import ExceptionWithStatusCode
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
+    # connect to db
+    conn_string = dbHandler().getConnString()
+
     try:
-        tasks = dbHandler().getTasks()
-        tasks = list(tasks)
-        return func.HttpResponse(json.dumps(tasks))
+        id = req.params.get('userId')
+
+        with pyodbc.connect(conn_string) as conn:
+            return getTasks(conn, id)
     except ExceptionWithStatusCode as err:
         return func.HttpResponse(str(err), status_code=err.status_code)
     except Exception as err:
         return func.HttpResponse(str(err), status_code=500)
+
+def getTasks(conn, id):
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT taskId, title, taskDescription, completed FROM Tasks WHERE taskUserId={}".format(id)
+        )
+        rows = cursor.fetchall()
+    for row in rows:
+        columns = [column[0] for column in cursor.description]
+        data = dict(zip(columns, row))
+
+    return func.HttpResponse(
+        body=data,
+        status_code=200,
+        mimetype='application/json'
+    )
--- functions/Utils/dbHandler.py
+++ functions/Utils/dbHandler.py
@@ -2,6 +2,7 @@ import os
 import pyodbc
 from ..Model.Task import Task
 from typing import List
+import logging
 
 SERVER = os.environ.get('SERVER')
 DATABASE = os.environ.get('DATABASE')
@@ -9,17 +10,11 @@ USERNAME = os.environ.get('USER')
 PASSWORD = os.environ.get('PASSWORD')
 DRIVER = os.environ.get('DRIVER')
 
-
-
 class dbHandler():
-    def __init__(self):
-        self.conn_string = "Driver={" + DRIVER + "};Server=" + SERVER + ";Database=" + DATABASE + ";Uid=" + USERNAME + ";Pwd={" + PASSWORD + "};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-        '''
-        'Driver=' + DRIVER + \
-            ';Server=' + SERVER + \
-            ';PORT=1433;Database=' + DATABASE + \
-            ';Uid='+USERNAME+';Pwd='+PASSWORD
-            '''
+    def getConnString(self):
+        self.conn_string = "Driver={};Server={};Database={};Uid={};Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
+        DRIVER, SERVER, DATABASE, USERNAME, PASSWORD)
+
 
    # [Task # 3] Query and return all tasks
     def getTasks(self) -> List[Task]:
