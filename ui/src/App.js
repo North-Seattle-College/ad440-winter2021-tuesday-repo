3f273ec8288ba92ffcc2995c138b06df5508809a
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2021-01-23 01:05:36 -0800

    Css stuff idk

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+import HomePage from "./Components/Pages/HomePage";
+
+import "./Components/css/App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Redirect to="/" exact />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ ui/src/Components/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../css/Home.css";
+
+const HomePage = (props) => {
+  return (
+    <div className="container">
+      <div className="header">Text</div>
+      <div className="body">
+        <div className="buttons">Text</div>
+        <div className="viewPort">Text</div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
