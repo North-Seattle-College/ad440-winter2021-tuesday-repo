78620a199ea05b21a6a105a07b19d7258f032be4
selinapn <57070340+selinapn@users.noreply.github.com>
2021-01-26 22:12:09 -0800

    added params to script

--- automation/functions/script.ps1
+++ automation/functions/script.ps1
@@ -1,9 +1,16 @@
+param(
+    [Parameter(Mandatory=$True)]
+    [string]
+    $resourceGroupName,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $location
+)
+
 #sign into azure account 
 Connect-AzAccount
 
-
-$resourceGroupName = Read-Host -Prompt "Name your resource group"
-$location = Read-Host -Prompt "Enter location"
 $templateUri = "https://raw.githubusercontent.com/selinapn/ad440-winter2021-tuesday-repo/automationspn/automation/functions/azuredeploy.json"
 
 #Create new resource group and deploy template
