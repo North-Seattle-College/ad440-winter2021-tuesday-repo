0fd01c6acc7c3e2355c73be74db2f363734d93ed
Phil <philipkim206@gmail.com>
2021-01-31 13:12:55 -0800

    added logging messaging to userId methods

--- functions/GetUserId/__init__.py
+++ functions/GetUserId/__init__.py
@@ -10,12 +10,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function for get user_id processed a request.')
 
     try:
+        logging.debug('Attempting a db connection...')
         requestHandler = RequestHandler()
+        logging.debug('Opened connection.')
         user_id = requestHandler.getUserId(req)
         dbHandler = dbHandler()
+        logging.debug(f'Attempting to execute GET user query for user {userId}')
         dbHandler.getUserId(user_id)
+        logging.debug(f'Executed GET query for {userId}')
         return func.HttpResponse("User ID found", status_code=201)
     except ExceptionWithStatusCode as err:
+        logging.error('No record with the requested parameters...')
         return func.HttpResponse(str(err), status_code=err.status_code)
     except Exception as err:
+        logging.error('Unable to fulfill request; try again...')
         return func.HttpResponse(str(err), status_code=500)
