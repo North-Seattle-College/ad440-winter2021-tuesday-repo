1329ab333138e5705d3023afb1f34814f04dbeef
Asia <Joanna.gromadzka@seattlecentral.edu>
2021-02-02 10:53:36 -0800

    outside resources with full ID

--- automation/vm/deploy-vm.ps1
+++ automation/vm/deploy-vm.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
   Creates a virtual machine and network interfase with one line cmdlet.
 .DESCRIPTION
-  This script will deploy a virtual machine and network interfase. 
+  This script will deploy a virtual machine and network interface. 
   This script  requires an existing Azure subscription and Resource group.
 .PARAMETER ResourceGroupName
     Required. Name of Resource group, if already exists will ask if add VM to it   
@@ -97,8 +97,37 @@ param (
 #To login to azure from powershell use the following
 Connect-AzAccount | Out-Null
 
+# $Credential = Get-Credential
+# Connect-AzAccount -Credential $Credential -Tenant $TenantId -ServicePrincipal
+
+# #checks if the resource group provided is valid
+# if(!$ResourceGroupName) {
+#     Write-Host “Resource group ‘$ResourceGroupName’ does not exist. Creating a resource group with given name and location"
+#     New-AzResourceGroup -Name $ResourceGroupName -Location $location 
+# } 
+# ##checks if the VirtualNetworkName provided is valid
+# if(!$VirtualNetworkName) {
+#     Write-Host “Given Virtual Network Name ‘$VirtualNetworkName’ does not exist. Creating a Virtual Network Name with given name and location"
+#     ../vnet/deploy-vnet.ps1
+# }
+# ##checks if the Public Ip Name provided is valid
+# if(!$PublicIpName) {
+#     Write-Host “Given Public Ip Name ‘$PublicIpName’ does not exist. Creating a Public Ip Name with given name and location"
+#     ../ip-address-script/create-ip.ps1
+# }
+# ##checks if the Storage Name provided is valid
+# if(!$StorageName) {
+#     Write-Host “Given Storage Name ‘$StorageName’ does not exist. Creating a Storage Name with given name and location"
+#     ../functions/create_azure_storageacct.ps1
+# }
 
 
+    
+
+
+
+    
+    
 
 $vmparameters = @{
     publicIpName = ($PublicIpName).ToLower();  
@@ -106,8 +135,12 @@ $vmparameters = @{
     networkInterfaceName = ($NetworkInterfaceName).ToLower();
     virtualNetworkName = ($VirtualNetworkName).ToLower(); 
     subnetName = ($SubNetName).ToLower();
+    resourceGroupName = ($ResourceGroupName).ToLower();
+    subscriptionId = ($SubscriptionId).ToLower();
+
     # storageName = 
 }
 $today = Get-Date -Format "MM-dd-yyyy"
 $templatePath = "./template.json"
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $templatePath -Location $location -TemplateParameterObject $vmparameters
+#   }
