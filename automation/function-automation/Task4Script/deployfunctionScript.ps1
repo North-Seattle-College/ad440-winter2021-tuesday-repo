f22e727fa7032b198b544fe62ae2bf93d33f5398
Raymond Wu <raymond.wu0114@gmail.com>
2021-02-02 17:55:43 -0800

    Update script to take templateUri

--- automation/function-automation/Task4Script/deployfunctionScript.ps1
+++ automation/function-automation/Task4Script/deployfunctionScript.ps1
@@ -17,16 +17,34 @@ param(
     [string]
     $location,     #Enter resource group location('westus2', 'westus')
 
+    # [Parameter(Mandatory=$True)]
+    # [string]
+    # $templateFile,  #Path of the template.json file,
+    
     [Parameter(Mandatory=$True)]
     [string]
-    $templateFile  #Path of the template.json file
+    $servicePrincipalId,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $servicePrincipalPassword,
 
 
+    [Parameter(Mandatory=$True)]
+    [string]
+    $subscriptionId,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $tenantId
 
 )
+$templateUri = "https://raw.githubusercontent.com/North-Seattle-College/ad440-winter2021-tuesday-repo/development/automation/function-automation/template.json"
+$securePassword = ConvertTo-SecureString -String $servicePrincipalPassword -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential($servicePrincipalId, $securePassword) 
 
-#Redirects the user to sign in to the Azure portal 
-Connect-AzAccount  
+#Connect to the account using the information 
+Connect-AzAccount -Credential $credentials -ServicePrincipal -Tenant $tenantId -SubscriptionId $subscriptionId
 
 #retrieve the given resource group
 Get-AzResourceGroup -Name $resourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
@@ -37,4 +55,4 @@ if ($notPresent)
 }
 
 #creates the azure function group
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile 
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri  $templateFile 
