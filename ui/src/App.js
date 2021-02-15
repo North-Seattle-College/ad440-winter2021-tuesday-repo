7864a8478dbb1d68c16d709967ea6815445ab500
RealDogDad <brandan@getfoster.net>
2021-02-15 12:37:55 -0800

    "Fixed some bugs"

--- ui/src/App.js
+++ ui/src/App.js
@@ -8,15 +8,8 @@ import {
 
 import HomeScreen from "./components/pages/HomeScreen";
 import UserScreen from "./components/pages/UserScreen";
-import TaskScreen from "./components/pages/TaskScreen";
 import NavScreen from "./components/navigation/NavScreen";
-import TaskListScreen from "./components/pages/TaskListScreen";
 import UsersListScreen from "./components/pages/UserListScreen";
-import ArtilleryListScreen from "./components/pages/ArtilleryListScreen";
-import ServerlessListScreen from "./components/pages/ServerlessListScreen";
-import ArtilleryDetailScreen from "./components/pages/ArtilleryDetailScreen";
-import ServerlessDetailScreen from "./components/pages/ServerlessDetailScreen";
-
 import "./components/css/App.css";
 
 const App = () => {
@@ -32,24 +25,6 @@ const App = () => {
           </Route>
           <Route path="/users/:userId" exact>
             <UserScreen />
-          </Route>
-          <Route path="/users/:userId/tasks" exact>
-            <TaskListScreen />
-          </Route>
-          <Route path="/users/:userId/tasks/:taskId" exact>
-            <TaskScreen />
-          </Route>
-          <Route path="/artillery" exact>
-            <ArtilleryListScreen />
-          </Route>
-          <Route path="/artillery/:testId" exact>
-            <ArtilleryDetailScreen />
-          </Route>
-          <Route path="/serverless" exact>
-            <ServerlessListScreen />
-          </Route>
-          <Route path="/serverless/:testId" exact>
-            <ServerlessDetailScreen />
           </Route>
           <Redirect to="/" exact />
         </Switch>
