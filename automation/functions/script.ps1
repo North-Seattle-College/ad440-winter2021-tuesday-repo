8b4fe8bd3164deb77acd5ccdf88e7b6da5c0bcb0
selinapn <57070340+selinapn@users.noreply.github.com>
2021-02-15 18:27:00 -0800

    s3

--- automation/functions/script.ps1
+++ automation/functions/script.ps1
@@ -12,15 +12,8 @@ param(
 Connect-AzAccount
 
 #Will change this to NSC raw link after merge
-$templateUri = "https://raw.githubusercontent.com/North-Seattle-College/ad440-winter2021-tuesday-repo/development/automation/functions/azuredeploy.json"
+$templateUri = "https://raw.githubusercontent.com/selinapn/ad440-winter2021-tuesday-repo/automationspn-sprint3/automation/functions/azuredeploy.json"
 
 #Create or check for existing resource group
-$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-if(!$resourceGroup)
-{
-    New-AzResourceGroup -Name $resourceGroupName -Location "$location"
-    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri
-}
-else{
-    Write-Host "Using existing resource group '$resourceGroupName'";
-}
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri
