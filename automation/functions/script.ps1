f054510c2c360c65a771226795bfcf60624f692c
selinapn <57070340+selinapn@users.noreply.github.com>
2021-02-15 23:55:06 -0800

    edt

--- automation/functions/script.ps1
+++ automation/functions/script.ps1
@@ -1,4 +1,4 @@
-<# param(
+ param(
     [Parameter(Mandatory = $True)]
     [string]
     $SubscriptionId,
@@ -16,24 +16,23 @@
     $ServicePrincipalPassword
 )
 
+
+#sign into azure account
 $securePassword = ConvertTo-SecureString -String $ServicePrincipalPassword -AsPlainText -Force;
 $credentials = New-Object -TypeName System.Management.Automation.PSCredential($ServicePrincipalId, $securePassword);
+Connect-AzAccount -Credential $credentials -Tenant $TenantId -ServicePrincipal
 
-#sign into azure account  #>
-Connect-AzAccount #-Credential $credentials -ServicePrincipal -Tenant $TenantId -SubscriptionId $SubscriptionId;
-
- 
 
 $parameters = @{
-    appName="nsc-fun-dev-usw2-tuesday";
-    storageName="nsctrdevusw2tuefun";
-    applicationInsight='nsc-appins-dev-usw2-Tuesday';
-    hostingPlan='nsc-asp-dev-usw2-tuesday';
-    appInsightsLocation="West US 2"
-}
+     appName="nsc-fun-dev-usw2-tuesday";
+     storageName="nsctrdevusw2tuefun";
+     applicationInsight='nsc-appins-dev-usw2-Tuesday';
+     hostingPlan='nsc-asp-dev-usw2-tuesday';
+     appInsightsLocation="West US 2"
+ }
 
 #Will change this to NSC raw link after merge
-$templateUri = "https://raw.githubusercontent.com/selinapn/ad440-winter2021-tuesday-repo/automationspn-sprint3/automation/functions/azuredeploy.json"
-$resourceGroupName = "nsc-rg-dev-usw2-tuesday"
+ $templateUri = "https://raw.githubusercontent.com/selinapn/ad440-winter2021-tuesday-repo/automationspn-sprint3/automation/functions/azuredeploy.json"
+ $resourceGroupName = "nsc-rg-dev-usw2-tuesday"
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterObject $parameters
+ New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterObject $parameters
