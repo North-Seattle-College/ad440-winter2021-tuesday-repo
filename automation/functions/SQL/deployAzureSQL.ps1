bc46a029ef9c0dac762b9b925c0a81cb2151e33e
vq37vhr <sunnylau1206@gmail.com>
2021-02-16 03:30:38 -0800

    updated codes to automate creating sql resources

--- automation/functions/SQL/deployAzureSQL.ps1
+++ automation/functions/SQL/deployAzureSQL.ps1
@@ -1,48 +1,44 @@
 param(
-    # Login parameters
-    [string] [Parameter(Mandatory=$true)] $tenantId,          
-    [string] [Parameter(Mandatory=$true)] $applicationId,
-    [string] [Parameter(Mandatory=$true)] $secret,
-    [string] [Parameter(Mandatory=$true)] $subscriptionId,
-    # Azure SQL server parameters
-    [string] [Parameter(Mandatory=$true)] $location,
-    [string] [Parameter(Mandatory=$true)] $resourceGroupName,
-    [string] [Parameter(Mandatory=$true)] $serverName,
-    [string] [Parameter(Mandatory=$true)] $administratorLogin,
-    [string] [Parameter(Mandatory=$true)] $administratorLoginPassword,
-    # Azure SQL db parameters
-    [string] [Parameter(Mandatory=$false)] $sqlDBName,    
-    # Tag parameters
-    [string] [Parameter(Mandatory=$true)] $createdBy,
-    [string] [Parameter(Mandatory=$true)] $creatorsEmail              
+        # Login parameters
+        [string] [Parameter(Mandatory = $true)] $tenantId,          
+        [string] [Parameter(Mandatory = $true)] $applicationId,
+        [string] [Parameter(Mandatory = $true)] $secret,
+        [string] [Parameter(Mandatory = $true)] $subscriptionId,
+        # Azure SQL server parameters
+        [string] [Parameter(Mandatory = $true)] $location,
+        [string] [Parameter(Mandatory = $true)] $resourceGroupName,
+        [string] [Parameter(Mandatory = $true)] $serverName,
+        [string] [Parameter(Mandatory = $true)] $administratorLogin,
+        [SecureString] [Parameter(Mandatory = $true)] $administratorLoginPassword,
+        # Azure SQL db parameters
+        [string] [Parameter(Mandatory = $false)] $sqlDBName,    
+        # Tag parameters
+        [string] [Parameter(Mandatory = $true)] $owner,
+        [string] [Parameter(Mandatory = $true)] $ownersEmail              
 )
 [securestring] $administratorLoginPassword = ConvertTo-SecureString $administratorLoginPassword -AsPlainText -Force
 
-# Log in and set the SubscriptionId in which to create these objects
 Import-Module ..\Login
 Login $tenantId $applicationId $secret $subscriptionId
 
-$pathToAzSqlTemplate = "./template.json"
+$pathToAzSqlTemplate = "./deployAzure.json"
 
 # Check for or create a resource group
 $resourceGroupExists = (Get-AzResourceGroup -Name $resourceGroupName `
--ErrorVariable notPresent -ErrorAction SilentlyContinue).ResourceGroupName `
--eq $resourceGroupName 
-
+                -ErrorVariable notPresent -ErrorAction SilentlyContinue).ResourceGroupName `
+        -eq $resourceGroupName 
 if (!$resourceGroupExists) {
-        New-AzResourceGroup -Name $resourceGroupName -Location "$location" -Force
-        Write-Host "Created resource group $resourceGroupName"
-} else {
-        Write-Host "Resource group $resourceGroupName already exists."
+        New-AzResourceGroup -Name $resourceGroupName -Location "$location" -TemplateUri "$pathToAzSqlTemplate" -Force
+        Write-Host "Creating new resource group $resourceGroupName"
+}
+else {
+        Write-Host "Use existing resource group $resourceGroupName"
 }
 
 # Deploy template
-Write-horceGroupDeployment -ResourceGroupName $resourceGroupName `
--TemplateFile $pathToAzSqlTemplate -administratorLogin $administratorLogin `
--administrst "Creating primary server and db..."
-New-AzResouatorLoginPassword $administratorLoginPassword -serverName $serverName `
--sqlDBName $sqlDBName -location $location -createdBy $createdBy -creatorsEmail $creatorsEmail
-
-
-# Clear deployment 
-# Remove-AzResourceGroup -ResourceGroupName $resourceGroupName
+Write-host "Creating primary server and db"
+New-AzResourceGroupDeployment 
+-ResourceGroupName $resourceGroupName `
+        -serverName $serverName `
+        -sqlDBName $sqlDBName `
+        -location $location
