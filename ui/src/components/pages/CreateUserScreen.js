bc6c41a635de125bc1a7cd93d6bca77280dd60cc
YAG0731 <57081128+YAG0731@users.noreply.github.com>
2021-02-23 16:54:45 -0800

    Update CreateUserScreen.js

--- ui/src/components/pages/CreateUserScreen.js
+++ ui/src/components/pages/CreateUserScreen.js
@@ -1,40 +1,29 @@
 import React, { useState, useEffect } from "react";
-// import express from 'express';
-
-const url = "https://nsc-func-dev-usw2-tuesday.azurewebsites.net/api/users";
-const config = {
-    url,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    }
-}
 
 const CreateUserScreen = (props) => {
-    const [firstname, setFirstName] = useState();
-    const [lastname, setLastName] = useState();
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
     const [email, setEmail] = useState();
     const queryString = require('query-string');
 
     const handleSubmit = (event) => {
         console.log(`
-        FirstName: ${firstname}
-        LastName: ${lastname}
+        FirstName: ${firstName}
+        LastName: ${lastName}
         Email: ${email}
         `);
         fetch("https://nsc-func-dev-usw2-tuesday.azurewebsites.net/api/users", {
             method: "POST",
             headers: {
-                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
                 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                firstname,
-                lastname,
+                firstName,
+                lastName,
                 email
-
             })
         })
         event.preventDefault();
@@ -47,9 +36,9 @@ const CreateUserScreen = (props) => {
             <label>
                 First Name:
             <input
-                    name="firstname"
-                    type="firstname"
-                    value={firstname}
+                    name="firstName"
+                    type="firstName"
+                    value={firstName}
                     onChange={e => setFirstName(e.target.value)}
                     required />
             </label>
@@ -57,9 +46,9 @@ const CreateUserScreen = (props) => {
             <label>
                 Last Name:
             <input
-                    name="lastname"
-                    type="lastname"
-                    value={lastname}
+                    name="lastName"
+                    type="lastName"
+                    value={lastName}
                     onChange={e => setLastName(e.target.value)}
                     required />
             </label>
