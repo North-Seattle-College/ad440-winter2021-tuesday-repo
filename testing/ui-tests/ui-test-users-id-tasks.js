6e0a0c961f2477a6c909cfca01e36167cfac8336
Jennvillacis <jennvillacis@gmail.com>
2021-02-25 00:06:29 -0800

    updated scripts
    working and taking a screen shot, unsure if correct. Unable to click by button since most of the buttons do not have a class. Not sure if this is what the test should look like.

--- testing/ui-tests/ui-test-users-id-tasks.js
+++ testing/ui-tests/ui-test-users-id-tasks.js
@@ -1,16 +1,33 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+
+const escapeXpathString = str => {
+  const splitedQuotes = str.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
+
+
+const clickByText = async (page, text) => {
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(`//a[contains(text(), ${escapedText})]`);
+
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(`Link not found: ${text}`);
+  }
+};
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://nscstrdevusw2tuecommon.z5.web.core.windows.net/index.html');
-    await page.screenshot({path: '/user/1/tasks'});
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://nscstrdevusw2tuecommon.z5.web.core.windows.net');
 
-    await browser.close();
+  await clickByText(page, `Users`)
+  await clickByText(page, `wgrinston0`)
+  await clickByText(page, `View User Tasks`)
+
+  await page.screenshot({ path: 'capture-users-id-tasks.png' });
+
+  await browser.close();
 
 })();
-
-/* Not tested just trying out code, unsure of goto link
-local host or static site? 
-using https://itnext.io/getting-started-using-puppeteer-headless-chrome-for-end-to-end-testing-8487718e4d97
-*/
