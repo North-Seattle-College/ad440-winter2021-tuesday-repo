36f2a0e63c2cfa57eff280dbb1e8fc88794dd364
Asia <Joanna.gromadzka@seattlecentral.edu>
2021-02-28 23:29:48 -0800

    test add screenshot

---
+++ testing/ui-tests/ui-test-users-id-tasks-id.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+    (async () => {
+      const browser = await puppeteer.launch({headless: true});
+      const page = await browser.newPage();
+    
+      await page.goto('https://nscstrdevusw2tuecommon.z5.web.core.windows.net/user/5/tasks/6');
+      
+      let date = new Date();
+      let stringDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+      await page.screenshot({path: './results/ad440-users-5-tasks-6-screenshot' + stringDate + '.png'});
+
+      await browser.close();
+    })();
