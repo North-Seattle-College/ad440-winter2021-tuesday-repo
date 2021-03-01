2fff710c66d5751d4af49df5e46d37f8fa2df9ba
OyunominJuno <oyunominmun@gmail.com>
2021-02-28 16:53:05 -0800

    screenshot is being saved into results directory

--- testing/ui-tests/ui-test-users-userid.js
+++ testing/ui-tests/ui-test-users-userid.js
@@ -3,6 +3,6 @@ const puppeteer = require('puppeteer');
       const browser = await puppeteer.launch({headless: true});
       const page = await browser.newPage();
       await page.goto('https://nscstrdevusw2tuecommon.z5.web.core.windows.net/users/46');
-      await page.screenshot({path: 'ad440-users-46-screenshot.png'});
+      await page.screenshot({path: '../results/ad440-users-46-screenshot.png'});
       await browser.close();
     })();
