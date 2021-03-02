50874cb97bf8ba075180ff16e70b488c1c73d5eb
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2021-03-02 14:00:54 -0800

    Delete user-userId-tasks.js

--- testing/headless-chrome/user-userId-tasks.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('<url goes here>');
-    await page.screenshot({ path: '<name of output file goes here>' })
-})
