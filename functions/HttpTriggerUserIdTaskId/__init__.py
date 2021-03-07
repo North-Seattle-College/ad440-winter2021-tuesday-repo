da18d0da2ec62c6bf2878c9c1f77b33377c889f7
All-Schuck-Up <schuckrw@gmail.com>
2021-03-07 10:53:59 -0800

    redis cache userId/tasks/taskId

--- functions/HttpTriggerUserIdTaskId/__init__.py
+++ functions/HttpTriggerUserIdTaskId/__init__.py
@@ -7,7 +7,10 @@ import azure.functions as func
 from ..Utils.dbHandler import dbHandler
 from ..Utils.ExceptionWithStatusCode import ExceptionWithStatusCode
 
-# Setup redis server
+# GLOBAL VARIABLES
+CACHE_TOGGLE = True #os.environ.get('CACHE_TOGGLE')
+USERID_TASKS_CACHE = b'users:{userId}:tasks:all'
+USERID_TASKID_CACHE = b'users:{userId}/tasks/{taskId}'
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -30,34 +33,37 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(str(err), status_code=500)
     logging.debug("Connection to DB successful!")
 
+    # Setup redis server
+    r = setupRedis()
+
     try:
         userId = req.route_params.get('userId')
         taskId = req.route_params.get('taskId')
-        with conn.cursor() as cursor:
-            logging.debug('Checking if task is in DB taskId: ' + taskId)
-            row = get_task_row(cursor, taskId)
-            if not row:
-                logging.debug('Task not found')
-                return func.HttpResponse(
-                    'Task not found',
-                    status_code=404
-                )
+        #with conn.cursor() as cursor:
+         #   logging.debug('Checking if task is in DB taskId: ' + taskId)
+          #  row = get_task_row(cursor, taskId)
+           # if not row:
+            #    logging.debug('Task not found')
+             #   return func.HttpResponse(
+              #      'Task not found',
+               #     status_code=404
+                #)
 
         # Return results according to the method
         if method == "GET":
             logging.debug("Attempting to retrieve task: " + taskId)
-            getTaskById = getTask(cursor, row)
+            getTaskById = getTask(conn, taskId, r)
             logging.debug("Task retrieved successfully!")
             return getTaskById
         elif method == "PUT":
             logging.debug("Attempting to update taskId: " + taskId)
             task_req_body = req.get_json()
-            new_task_id_http_response = updateTask(conn, task_req_body)
+            new_task_id_http_response = updateTask(conn, task_req_body, r)
             logging.debug("Task added successfully!")
             return new_task_id_http_response
         elif method == "DELETE":
             logging.debug('Attempting to delete taskId: ' + taskId)
-            return deleteTask(cursor, taskId)
+            return deleteTask(conn, taskId, r)
         else:
             logging.warn(f"Request with method {method} has been recieved, but that is not allowed for this endpoint")
             return func.HttpResponse(status_code=405)
@@ -69,19 +75,48 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         conn.close()
         logging.debug('Connection to DB closed')
 
-def getTask(cursor, row):
-    logging.debug('Retrieving task by Id...')
-    columns = [column[0] for column in cursor.description]
-    data = dict(zip(columns, row))
+def getTask(conn, row, r):
+    try:
+        cache = getTaskIdCache(r)
+    except TypeError as err:
+        logging.debug(err.args[0])
 
-    logging.debug('Task retrieved successfully')
-    return func.HttpResponse(
-        json.dumps(data),
-        status_code=200,
-        mimetype='application/json'
-    )
+    if cache:
+        logging.debug('Data found in cache...')
+        return func.HttpResponse(
+            cache.decode('utf-8'),
+            status_code=200,
+            mimetype='application/json'
+        )
+    else:
+        if(CACHE_TOGGLE == True):
+            logging.debug('cache is empty searching database...')
+            
+        with conn.cursor() as cursor:
+            logging.debug('Using connection to execute sql query for taskId...')
 
-def updateTask(cursor, task_req_body):
+            row = get_task_row(cursor, taskId)
+            if not row:
+                logging.debug('Task not found')
+                return func.HttpResponse(
+                    'Task not found',
+                    status_code=404
+                )
+
+            columns = [column[0] for column in cursor.description]
+            data = dict(zip(columns, row))
+
+            # Cache taskId data
+            cacheTaskId(r, taskId)
+
+            logging.debug('Task retrieved successfully')
+            return func.HttpResponse(
+                json.dumps(data),
+                status_code=200,
+                mimetype='application/json'
+            )
+
+def updateTask(conn, task_req_body, r):
     task_req_body = req.get_json()
 
     # Validation checks on required fields
@@ -91,24 +126,90 @@ def updateTask(cursor, task_req_body):
     except AssertionError as err:
         logging.error('User request body did not contain the necessary fields!')
         return func.HttpResponse(
-            'Task updated',
+            err.args[0], 
+            status_code=400
+        )
+
+    logging.debug('Request body contains all the necessary fields!')
+    with conn.cursor() as cursor:
+        title = task_req_body['title']
+        taskDescription = task_req_body['taskDescription']
+        taskParams = (title, taskDescription)
+
+        # update in database
+        updateTaskQuery = 'UPDATE [dbo].[tasks] SET title = ?, taskDescription = ? WHERE userId = ? AND taskId = ?'
+        executeQuery = cursor.execute(updateTaskQuery, taskParams)
+        if not executeQuery:
+            logging.error('There was an ERROR updating this task or it does NOT exist.')
+            return func.HttpResponse(
+                'Bad or invalid input',
+                status_code=404
+            )
+
+        # Clear taskId cache
+        clearTaskIdCache(r)
+
+        logging.debug('Task updated successfully')
+        return func.HttpResponse(
+            'Success',
             status_code=200
         )
 
-
-def deleteTask(cursor, taskId):
+def deleteTask(conn, taskId, r):
     logging.debug('Attempting to delete taskId: ' + taskId)
-    delete_task_query = 'DELETE FROM tasks WHERE taskId={}'.format(taskId)
-    logging.debug('Executing delete query')
-    cursor.execute(delete_task_query, (taskId))
-    logging.debug('task deleted')
-    return func.HttpResponse(
-        'task deleted',
-        status_code=200
-    )
+    with conn.cursor() as cursor:
+        delete_task_query = 'DELETE FROM tasks WHERE taskId={}'.format(taskId)
+        logging.debug('Executing delete query')
+        cursor.execute(delete_task_query, (taskId))
+        logging.debug('task deleted')
+
+        clearTaskIdCache(r)
+
+        return func.HttpResponse(
+            'task deleted',
+            status_code=200
+        )
 
 def get_task_row(cursor, taskId):
     cursor.execute(
         'SELECT taskId, taskUserId, title, taskDescription FROM tasks WHERE taskId={}'.format(taskId)
     )
     return cursor.fetchone()
+
+# CONN STRING : nsc-redis-dev-usw2-tuesday.redis.cache.windows.net:6380,password=I6yuDCJzGBaR55KyI8nb30leOvFYpIEv3HTlgsir+xU=,ssl=True,abortConnect=False
+def setupRedis():
+    REDIS_HOST = 'nsc-redis-dev-usw2-tuesday.redis.cache.windows.net'
+    REDIS_KEY = 'I6yuDCJzGBaR55KyI8nb30leOvFYpIEv3HTlgsir+xU'
+    REDIS_PORT = '6380'
+
+    return redis.StrictRedis(
+        host= REDIS_HOST,
+        port= REDIS_PORT,
+        db= 0,
+        password= REDIS_KEY,
+        ssl= True
+    )
+
+def cacheTaskId(r, taskId):
+    if(CACHE_TOGGLE == True):
+        try:
+            logging.debug('Caching taskId - {taskId} ...')
+            r.set(USERID_TASKID_CACHE, json.dumps(taskId), ex=1200)
+            logging.debug('Caching complete')
+        except Exception as err:
+            logging.debug('Caching failed')
+            logging.debug(err.args[0])
+
+def getTaskIdCache(r):
+    if(CACHE_TOGGLE == True):
+        logging.debug('Checking cache for taskId...')
+        try:
+            cache = r.get(USERID_TASKID_CACHE)
+            return cache
+        except ExceptionWithStatusCode as err:
+            logging.debug('Failed to fetch taskId from cache')
+            return func.HttpResponse(str(err), status_code=err.status_code) 
+
+def clearTaskIdCache(r):
+    r.delete(USERID_TASKID_CACHE)
+    logging.debug('TaskId Cache deleted successfully')
