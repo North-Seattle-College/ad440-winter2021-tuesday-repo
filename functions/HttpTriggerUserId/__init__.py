ccbd674ad8d2b4c02a56a81c480ac485fecb300e
All-Schuck-Up <schuckrw@gmail.com>
2021-03-08 14:35:26 -0800

    Update __init__.py

--- functions/HttpTriggerUserId/__init__.py
+++ functions/HttpTriggerUserId/__init__.py
@@ -33,7 +33,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         userId = req.route_params.get('userId')
         with conn.cursor() as cursor:
-            #logging.debug('Checking for user in database: ' + userId)
+            logging.debug('Checking for user in database: ' + userId)
             row = get_user_row(cursor, userId)
             if not row:
                 logging.debug('User not found')
@@ -68,9 +68,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 # Get target user by ID
 def getUserById(cursor, row):
     logging.debug("Attempting to retrieve user by ID...")
-    # This will convert the results from the query into json properties.
-    # More information can be found on the link below:
-    # https://stackoverflow.com/questions/16519385/output-pyodbc-cursor-results-as-python-dictionary/16523148#16523148
     columns = [column[0] for column in cursor.description]
     data = dict(zip(columns, row))
 
@@ -100,10 +97,14 @@ def updateUser(req, cursor, userId):
         assert "firstName" in user_req_body, "User request body did not contain field: 'firstName'"
         assert "lastName" in user_req_body, "User request body did not contain field: 'lastName'"
         assert "email" in user_req_body, "User request body did not contain field: 'email'"
-    except AssertionError as user_req_body_content_error:
+    except AssertionError as err:
         logging.error(
             "User request body did not contain the necessary fields!")
-        return func.HttpResponse(user_req_body_content_error.args[0], status_code=400)
+        return func.HttpResponse(
+            err.args[0], 
+            status_code=400
+        )
+
     logging.debug("User request body contains all the necessary fields!")
 
     # Unpack user data
@@ -134,7 +135,5 @@ def deleteUser(cursor, userId):
     )
 
 def get_user_row(cursor, userId):
-    cursor.execute(
-        'SELECT userId, email, userPassword, firstName, lastName FROM users WHERE userId={}'.format(userId)
-    )
+    cursor.execute('SELECT userId, email, userPassword, firstName, lastName FROM users WHERE userId={}'.format(userId))
     return cursor.fetchone()
