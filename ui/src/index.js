fb05b7142b8b3840a7ebc53827345ad279b9d078
OyunominJuno <oyunominmun@gmail.com>
2021-03-14 18:42:32 -0700

    added a hashrouting for the app.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {HashRouter} from 'react-router-dom';
 import "./components/css/Index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
